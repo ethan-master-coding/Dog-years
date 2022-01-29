99e41411efee0022bd8086e098653177952d747e
Ethan <92686703+Ethan-Master-Coding@users.noreply.github.com>
2022-01-29 11:23:20 -0500

    Added the code

---
+++ dog years.js
@@ -0,0 +1,13 @@
+let myAge = 69;
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+laterYears = myAge-2;
+
+laterYears *= 4;
+
+myAgeInDogYears = earlyYears+laterYears;
+
+toLowerCase = "Ethan".toLowerCase()
+
+console.log(`My name is ${toLowerCase}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
